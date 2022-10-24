bcd6bb22b28030e7d2927369fc7cc64878e4c473
Homayoon Ranjbar <horanj.1022@gmail.com>
2022-10-23 23:45:04 -0500

    Update restoreBinaryTree.cpp

--- restoreBinaryTree.cpp
+++ restoreBinaryTree.cpp
@@ -1,19 +1,15 @@
-Tree<int> * subTree(vector<int> & in, vector<int>::iterator inst, vector<int>::iterator inend, vector<int> & pre, vector<int>::iterator prest, vector<int>::iterator preend){
-    if (distance(inst, inend) == 1) return new Tree<int>(*prest);
-    else if (distance(inst, inend) == 2) {
-        Tree<int> * root = new Tree<int>(*prest);
-        root->left = new Tree<int>(*inst);
-        return root;
-    } else {
-        Tree<int> * root = new Tree<int>(*prest);
-        vector<int>::iterator rootIt = find(inst, inend, *prest);
-        int dist = distance(inst, rootIt);
-        root->left = subTree(in, inst, rootIt, pre, prest + 1, prest + 1 + dist);
-        root->right = subTree(in, rootIt + 1, inend, pre, prest + 1 + dist, preend);
-        return root;
-    }
+Tree<int> * subTree(vector<int> & in, int inst, int inend, vector<int> & pre, int prest, int preend){
+    if (inend < inst || preend < prest) return nullptr;
+    Tree<int> * root = new Tree<int>(pre[prest]);
+    int rootVal = pre[prest];
+    int ix = inst;
+    while (in[ix] != rootVal) ix++;
+    int sz = ix - inst;
+    root->left = subTree(in, inst, ix - 1, pre, prest + 1, prest + sz);
+    root->right = subTree(in, ix + 1, inend, pre, prest + 1 + sz, preend);
+    return root;
 }
 
 Tree<int> * solution(vector<int> inorder, vector<int> preorder) {
-    return subTree(inorder, inorder.begin(), inorder.end(), preorder, preorder.begin(), preorder.end());
+    return subTree(inorder, 0, inorder.size() - 1, preorder, 0, preorder.size() - 1);
 }
