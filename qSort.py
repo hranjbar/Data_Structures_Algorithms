5c91dc80cfe2ed1e5c0e0658644ab898596d8975
Homayoon Ranjbar <horanj.1022@gmail.com>
2022-10-29 16:32:31 -0500

    Update qSort.py
    Added main to test

--- qSort.py
+++ qSort.py
@@ -26,3 +26,9 @@ def qSort(a, l, r):
     qSort(a, i, r);
 
     return a
+
+if __name__ == "__main__":
+    ar = [-15,5,3,-17,-10,84,21,19,-6,0,-22,9,18,2,43,90,56,-1,21,36]
+    print(f"sample array: {ar}")
+    ar = qSort(ar, 0, len(ar)-1)
+    print(f"sorted array: {ar}")
