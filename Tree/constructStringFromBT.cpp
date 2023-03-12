971f4fe22d1aa4e5420d485eb8e9b5035fca3613
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-03-12 17:40:05 -0500

    Create constructStringFromBT.cpp
    Leetcode # 606. Construct String from Binary Tree

---
+++ Tree/constructStringFromBT.cpp
@@ -0,0 +1,27 @@
+/*
+Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+
+Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if (!root) return "";
+        string ans = to_string(root->val);
+        if (root->right) ans += "(" + tree2str(root->left) + ")(" + tree2str(root->right) + ")";
+        else if (root->left) ans += "(" + tree2str(root->left) + ")";
+        return ans;
+    }
+};
