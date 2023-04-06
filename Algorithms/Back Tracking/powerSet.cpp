439746415df5d7badde2b003f9d5d482a07d8774
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-04-05 22:58:26 -0500

    Update powerSet.cpp
    No need to loop for k. 
    where k is the size of subset

--- Algorithms/Back Tracking/powerSet.cpp
+++ Algorithms/Back Tracking/powerSet.cpp
@@ -9,18 +9,16 @@ The solution set must not contain duplicate subsets. Return the solution in any 
 class Solution {
 public:
     vector<vector<int>> ans;
-    void bk(vector<int> & nums, int & k, int l = 0, vector<int> subset = {}){
-        if (k == subset.size()) ans.push_back(subset);
-        else {
-            for (int i = l; i < nums.size(); i++){
-                subset.push_back(nums[i]);
-                bk(nums, k, i + 1, subset);
-                subset.pop_back();
-            }
+    void bk(vector<int> & nums, int l = 0, vector<int> subset = {}){
+        ans.push_back(subset);
+        for (int i = l; i < nums.size(); i++){
+            subset.push_back(nums[i]);
+            bk(nums, i + 1, subset);
+            subset.pop_back();
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        for (int k=0; k <= nums.size(); k++) bk(nums, k);
+        bk(nums);
         return ans;
     }
 };
