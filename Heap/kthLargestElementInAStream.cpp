a9cc7fbda5d86e8b4daf1c7428348a0a7402080c
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-12-04 21:30:44 -0600

    LeetCode # 703. Kth largest element in a stream

--- Heap/kthLargestElementInAStream.cpp
+++ Heap/kthLargestElementInAStream.cpp
@@ -1,34 +1,34 @@
 /*
-Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Design a class to find the kth largest element in a stream. 
+Note that it is the kth largest element in the sorted order
+, not the kth distinct element.
 
 Implement KthLargest class:
 
-KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
-int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+KthLargest(int k, int[] nums) Initializes the object with 
+the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and
+returns the element representing the kth largest element 
+in the stream.
 */
 
 class KthLargest {
 public:
-    priority_queue<int, vector<int>, greater<int>> minq;
-    priority_queue<int> maxq;
-    KthLargest(int k, vector<int>& nums) {
-        for (int n : nums) maxq.push(n);
-        for (int i=0; i<k; i++) {
-            if (!maxq.empty()){
-                minq.push(maxq.top());
-                maxq.pop();
-            }
-            else minq.push((0x1<<31)); // make sure minq has at least k elements
-        }
+    KthLargest(int k, vector<int>& nums) : k_(k){
+        int n=nums.size();
+        for (int i=0; i<n; i++) add(nums[i]);
     }
     
     int add(int val) {
-        if (val > minq.top()){
-            minq.push(val);
-            minq.pop();
+        if (minHeap.size() < k_) minHeap.push(val);
+        else if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
         }
-        return minq.top();
+        return minHeap.top();
     }
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int k_;
 };
 
 /**
