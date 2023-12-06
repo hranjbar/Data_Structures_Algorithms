040cb2c3eeea2da2673e875b802d2a4c375fd463
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-12-06 09:45:48 -0600

    LeetCode # 912. Sort an array (Heap Sort O(1) extra memory)

--- Algorithms/Heap Sort/heapSortHeapify.cpp
+++ Algorithms/Heap Sort/heapSortHeapify.cpp
@@ -1,25 +1,37 @@
 /*
 Given an array of integers nums, sort the array in ascending order and return it.
 
-You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+You must solve the problem without using any built-in functions in O(nlog(n))
+time complexity and with the smallest space complexity possible.
 */
 
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        len = nums.size();
-        for (int j = len - 1; j >= 0; j--) sinkOne(nums, j);
-        while (len > 0) {
-            int num = nums[0];
-            pop(nums);
-            nums[len] = num;
+        // vector<int> sorted;
+        int len = nums.size();
+        // for (int& n : nums) insert(n, sorted, len);
+        for (int j = len - 1; j >= 0; j--) heapify(nums, j);
+        for (int j = nums.size() - 1; j >= 0; j--) {
+            int num = pop(nums, len);
+            nums[j] = num;
         }
         return nums;
     }
 private:
     // implement max heap
-    int len;
-    void sinkTop(vector<int>& nums)
+    void swim(vector<int>& nums, int& len)
+    {
+        int i = len - 1;
+        while (i >= 0) {
+            int root = (i - 1) / 2;
+            if (nums[i] > nums[root]) {
+                swap(nums[i], nums[root]);
+                i = root;
+            } else break;
+        }
+    }
+    void sink(vector<int>& nums, int& len)
     {
         int i = 0;
         while (i < len) {
@@ -39,8 +51,9 @@ private:
             } else break;
         }
     }
-    void sinkOne(vector<int>& nums, int i)
+    void heapify(vector<int>& nums, int i)
     {
+        int len = nums.size();
         while (i < len) {
             int l = 2 * i + 1, r = 2 * i + 2;
             int bigger;
@@ -58,27 +71,18 @@ private:
             } else break;
         }
     }
-    void pop(vector<int>& nums)
+    void insert(int num, vector<int>& nums, int& len)
     {
+        nums.push_back(num);
+        len++;
+        swim(nums, len);
+    }
+    int pop(vector<int>& nums, int& len)
+    {
+        int ret = nums[0];
         nums[0] = nums[len - 1];
         len--;
-        sinkTop(nums);
+        sink(nums, len);
+        return ret;
     }
-    // void swim(vector<int>& nums)
-    // {
-    //     int i = len - 1;
-    //     while (i >= 0) {
-    //         int root = (i - 1) / 2;
-    //         if (nums[i] > nums[root]) {
-    //             swap(nums[i], nums[root]);
-    //             i = root;
-    //         } else break;
-    //     }
-    // }
-    // void insert(int num, vector<int>& nums)
-    // {
-    //     nums.push_back(num);
-    //     len++;
-    //     swim(nums);
-    // }
 };
