2b8832fdf7409f85b1809d1c5ebe4bcea36fc36b
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-12-08 11:58:53 -0600

    LeetCode # 109. Converted sorted linked-list to binary search tree

---
+++ Tree/BST/convertSortedListToBST.py
@@ -0,0 +1,48 @@
+'''
+Given the head of a singly linked list where elements 
+are sorted in ascending order, convert it to a 
+height-balanced
+ binary search tree.
+
+Example 1:
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5],
+which represents the shown height balanced BST.
+
+Example 2:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in head is in the range [0, 2 * 104].
+-105 <= Node.val <= 105
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+        dum = ListNode()
+        dum.next = head
+        slow, fast = dum, dum
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        root = TreeNode(slow.next.val)
+        root.right = self.sortedListToBST(slow.next.next)
+        slow.next = None
+        root.left = self.sortedListToBST(head)
+        return root
