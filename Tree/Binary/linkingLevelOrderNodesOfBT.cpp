7ffcc8b7c8293faa0340abdf0ef1c1e03220b92b
Homayoon Ranjbar <horanj.1022@gmail.com>
2023-12-08 14:43:05 -0600

    Linking level-order nodes of a binary tree

--- Tree/Binary/linkingLevelOrderNodesOfBT.cpp
+++ Tree/Binary/linkingLevelOrderNodesOfBT.cpp
@@ -1,5 +1,7 @@
 /*
-You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+You are given a perfect binary tree where all leaves are on 
+the same level, and every parent has two children. The binary
+tree has the following definition:
 
 struct Node {
   int val;
@@ -7,11 +9,12 @@ struct Node {
   Node *right;
   Node *next;
 }
-Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Populate each next pointer to point to its next right node.
+If there is no next right node, the next pointer should be
+set to NULL.
 
 Initially, all next pointers are set to NULL.
 */
-
 
 /*
 // Definition for a Node.
