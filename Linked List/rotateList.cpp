16d45f3845fe6a865ecc0170f5a834a695c10b51
Homayoon Ranjbar <horanj.1022@gmail.com>
2024-01-18 20:54:35 -0600

    Leetcode # 61. Rotate List

--- Linked List/rotateList.cpp
+++ Linked List/rotateList.cpp
@@ -1,45 +1,37 @@
 /*
 Given the head of a linked list, rotate the list to the right by k places.
-*/
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+Example 2:
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+Constraints:
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
  */
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (!head) return nullptr;
-        ListNode * node = head;
-        int n=0; 
-        while (node) {
-            n++;
-            node = node->next;
-        }
-        k = k % n;
-        if (k != 0){
-            ListNode * dummy = new ListNode();
-            dummy->next = head;
-            ListNode * node = dummy;
-            for (int i=k; i<n; i++) node = node->next;
-            ListNode * h2 = node->next; // remainder of head is h2
-            node->next = nullptr; // cut remainder of head
-            
-            // append head to h2
-            dummy = new ListNode();
-            dummy->next = h2;
-            node = dummy;
-            for (int i=0; i<k; i++) node = node->next;
-            node->next = head;
-           
-            return h2;
-        }
-        return head;
+        if (!head) return nullptr;  // trivial
+        int len=0;
+        ListNode* cur=head;
+        while(cur) {cur=cur->next; len++;} // find length of list
+        k=k%len; // modify k
+        if (k==0) return head; // trivial
+        ListNode* dum=new ListNode(0,head);
+        cur=dum;
+        for(int i=0; i<len-k; i++) cur=cur->next; // find link before new head
+        dum=new ListNode(0,cur->next); // dum->next will be new head
+        cur->next=nullptr; // kill cycle
+        cur=dum;
+        while(cur->next) cur=cur->next; // find old tail
+        cur->next=head; // point old tail to old head
+        return dum->next;
     }
 };
