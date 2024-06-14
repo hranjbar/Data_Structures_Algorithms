6e6c1f40755e69585430defdfdc8d48f2a497062
Homayoon Ranjbar <horanj.1022@gmail.com>
2024-06-13 20:42:55 -0500

    LeetCode # 114. Flatten Binary Tree to Linked List

--- Tree/Binary/flatten_BT.cpp
+++ Tree/Binary/flatten_BT.cpp
@@ -38,19 +38,36 @@ Follow up: Can you flatten the tree in-place (with O(1) extra space)?
  left(left), right(right) {}
  * };
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     void flatten(TreeNode* root) {
+        // termination
         if (!root) return;
-        TreeNode *l=root->left, *r=root->right;
-        flatten(r);
-        if (l) {
-            flatten(l);
-            root->left = nullptr;
-            root->right = l;
-            TreeNode* cur = l;
-            while (cur && cur->right) cur=cur->right;
-            cur->right = r;
-        }
+
+        // right
+        flatten(root->right);
+        TreeNode* r = root->right;
+
+        // left
+        flatten(root->left);
+
+        // link
+        root->right = root->left;
+        root->left = nullptr;
+        TreeNode* cur = root;
+        while (cur->right) cur = cur->right;
+        cur->right = r;
     }
 };
