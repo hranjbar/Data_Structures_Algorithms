9749d95c11901c1c8e0fb99bf55b02d6edcc29c3
Homayoon Ranjbar <horanj.1022@gmail.com>
2024-06-17 18:06:38 -0500

    LeetCode # 841. Keys and Rooms

--- Graph/canVisitAllRooms.cpp
+++ Graph/canVisitAllRooms.cpp
@@ -1,27 +1,52 @@
 /*
-There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. 
-However, you cannot enter a locked room without having its key.
+There are n rooms labeled from 0 to n - 1 and all the rooms are locked 
+except for room 0. Your goal is to visit all the rooms. However, you 
+cannot enter a locked room without having its key.
+When you visit a room, you may find a set of distinct keys in it. Each 
+key has a number on it, denoting which room it unlocks, and you can 
+take all of them with you to unlock the other rooms.
+Given an array rooms where rooms[i] is the set of keys that you can 
+obtain if you visited room i, return true if you can visit all the 
+rooms, or false otherwise.
 
-When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, 
-and you can take all of them with you to unlock the other rooms.
+Example 1:
+Input: rooms = [[1],[2],[3],[]]
+Output: true
+Explanation: 
+We visit room 0 and pick up key 1.
+We then visit room 1 and pick up key 2.
+We then visit room 2 and pick up key 3.
+We then visit room 3.
+Since we were able to visit every room, we return true.
 
-Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
+Example 2:
+Input: rooms = [[1,3],[3,0,1],[2],[0]]
+Output: false
+Explanation: We can not enter room number 2 since the only key that 
+unlocks it is in that room.
+
+Constraints:
+n == rooms.length
+2 <= n <= 1000
+0 <= rooms[i].length <= 1000
+1 <= sum(rooms[i].length) <= 3000
+0 <= rooms[i][j] < n
+All the values of rooms[i] are unique.
 */
 
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_set<int> vis;
-        queue<int> q;
-        q.push(0);
+        int n = rooms.size();
+        vector<int> vis(n);
+        queue<int> q{{0}};
         while (!q.empty()) {
-            int i = q.front();
-            q.pop();
-            vis.insert(i);
-            for (int j : rooms[i]) {
-                if (vis.count(j) == 0) q.push(j);
+            int cur = q.front(); q.pop();
+            vis[cur] = 1;
+            for (int& to : rooms[cur]) {
+                if (vis[to] == 0) q.push(to);
             }
         }
-        return vis.size() == rooms.size();
+        return n == accumulate(vis.begin(), vis.end(), 0);
     }
 };
