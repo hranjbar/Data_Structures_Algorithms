fc28d11a6eded4d1167ee6f0d76b23bb6a60c110
Homayoon Ranjbar <horanj.1022@gmail.com>
2024-06-19 14:00:06 -0400

    LeetCode # 45. Jump Game II

--- Algorithms/DP/minNumJumps.cpp
+++ Algorithms/DP/minNumJumps.cpp
@@ -1,27 +1,25 @@
 /*
-You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
-
-Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
-
+You are given a 0-indexed array of integers nums of length n. 
+You are initially positioned at nums[0].
+Each element nums[i] represents the maximum length of a forward 
+jump from index i. In other words, if you are at nums[i], you can 
+jump to any nums[i + j] where:
 0 <= j <= nums[i] and
 i + j < n
-Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
-
- 
+Return the minimum number of jumps to reach nums[n - 1]. The test 
+cases are generated such that you can reach nums[n - 1].
 
 Example 1:
-
 Input: nums = [2,3,1,1,4]
 Output: 2
-Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
+Explanation: The minimum number of jumps to reach the last index 
+is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
+Example 2:
 Input: nums = [2,3,0,1,4]
 Output: 2
  
-
 Constraints:
-
 1 <= nums.length <= 104
 0 <= nums[i] <= 1000
 It's guaranteed that you can reach nums[n - 1].
@@ -30,17 +28,12 @@ It's guaranteed that you can reach nums[n - 1].
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n);
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] + i >= n - 1) dp[i] = 1;
-            else {
-                int minval = n;
-                for (int j = i + 1; j <= i + nums[i]; j++) {
-                    if (dp[j] < minval) minval = dp[j];
-                }
-                dp[i] = minval + 1;
-            }
+        int N = nums.size();
+        vector<int> dp(N);
+        for (int i = N - 2; i >= 0; i--) {
+            if (nums[i] + i >= N - 1) dp[i] = 1; // one jump from here to finish
+            else dp[i] = 1 + *min_element(dp.begin() + i + 1, dp.begin() + i + 1 + nums[i]);
+            // 1 jump from here to best point, then best # of jumps from there to finish
         }
         return dp.front();
     }
