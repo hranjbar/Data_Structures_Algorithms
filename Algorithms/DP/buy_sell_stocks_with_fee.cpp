025e3a607dab9ed787fb8a3cf99e2df056e44721
Homayoon Ranjbar <horanj.1022@gmail.com>
2024-06-19 21:16:16 -0400

    LeetCode # 714. Best Time to Buy and Sell Stock with Transaction Fee

--- Algorithms/DP/buy_sell_stocks_with_fee.cpp
+++ Algorithms/DP/buy_sell_stocks_with_fee.cpp
@@ -2,21 +2,16 @@
 You are given an array prices where prices[i] is the price of 
 a given stock on the ith day, and an integer fee representing 
 a transaction fee.
-
 Find the maximum profit you can achieve. You may complete as 
 many transactions as you like, but you need to pay the 
 transaction fee for each transaction.
-
 Note:
-
 You may not engage in multiple transactions simultaneously 
 (i.e., you must sell the stock before you buy again).
 The transaction fee is only charged once for each stock
 purchase and sale.
- 
 
 Example 1:
-
 Input: prices = [1,3,2,8,4,9], fee = 2
 Output: 8
 Explanation: The maximum profit can be achieved by:
@@ -25,14 +20,12 @@ Explanation: The maximum profit can be achieved by:
 - Buying at prices[4] = 4
 - Selling at prices[5] = 9
 The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
-Example 2:
 
+Example 2:
 Input: prices = [1,3,7,5,10,3], fee = 3
 Output: 6
  
-
 Constraints:
-
 1 <= prices.length <= 5 * 104
 1 <= prices[i] < 5 * 104
 0 <= fee < 5 * 104
@@ -44,9 +37,9 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n, vector<int>(2));
         dp.front() = {0-prices.front(), 0};
-        for (int i=1; i<n; i++) {
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i]);   // no fee when buying
-            dp[i][1] = max(dp[i-1][0] + prices[i] - fee, dp[i-1][1]); // pay fee when selling
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i]);   // no fee when buying
+            dp[i][1] = max(dp[i-1][0]+prices[i]-fee, dp[i-1][1]); // pay fee when selling
         }
         return max(dp.back()[0], dp.back()[1]);
     }
