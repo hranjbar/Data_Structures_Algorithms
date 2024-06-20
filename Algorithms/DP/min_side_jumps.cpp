448fe96267f18b46b45be4413fece5d2d58fb8a7
Homayoon Ranjbar <horanj.1022@gmail.com>
2024-06-19 23:32:32 -0400

    LeetCode # 1824. Minimum Side Jumps

--- Algorithms/DP/min_side_jumps.cpp
+++ Algorithms/DP/min_side_jumps.cpp
@@ -3,13 +3,11 @@ There is a 3 lane road of length n that consists of n + 1 points labeled
 from 0 to n. 
 A frog starts at point 0 in the second lane and wants to jump to point n. 
 However, there could be obstacles along the way.
-
 You are given an array obstacles of length n + 1 where each obstacles[i] 
 (ranging from 0 to 3) describes an obstacle on the lane obstacles[i] at 
 point i. 
 If obstacles[i] == 0, there are no obstacles at point i. 
 There will be at most one obstacle in the 3 lanes at each point.
-
 For example, if obstacles[2] == 1, then there is an obstacle on lane 1 
 at point 2.
 The frog can only travel from point i to point i + 1 on the same lane 
@@ -18,11 +16,9 @@ the lane at point i + 1. To avoid obstacles, the frog can also perform
 a side jump to jump to 
 another lane (even if they are not adjacent) at the same point if there
 is no obstacle on the new lane.
-
 For example, the frog can jump from lane 3 at point 3 to lane 1 at point 3.
 Return the minimum number of side jumps the frog needs to reach any lane at
 point n starting from lane 2 at point 0.
-
 Note: There will be no obstacles on points 0 and n.
 
 Example 1:
@@ -32,6 +28,7 @@ Explanation: The optimal solution is shown by the arrows above.
 There are 2 side jumps (red arrows).
 Note that the frog can jump over obstacles only when making side jumps 
 (as shown at point 2).
+
 Example 2:
 Input: obstacles = [0,1,1,3,3,0]
 Output: 0
@@ -53,25 +50,29 @@ obstacles[0] == obstacles[n] == 0
 class Solution {
 public:
     int minSideJumps(vector<int>& obstacles) {
-        int n = obstacles.size() - 1;
-        vector<vector<int>> dp(obstacles.size(), vector<int>(3, INT_MAX));
-        dp.back() = {0, 0, 0};  // no obstacles on point n
-        for (int i = n - 1; i >= 0; i--) {
-            for (int cur : {1, 2, 3}) {
-                if (cur != obstacles[i]) {
-                    if (obstacles[i + 1] != cur) dp[i][cur - 1] = dp[i + 1][cur - 1];
-                    else {
-                        int val = INT_MAX;
-                        for (int next : {1,2,3}) {
-                            if (obstacles[i] != next) val = min(val, dp[i+1][next-1]);
-                        }
-                        dp[i][cur-1] = 1 + val;
-                    }
-                }
-                // printf("(%i,%i): [%i,%i,%i]\n",i, cur, dp[i][0], dp[i][1], dp[i][2]);
+        int N = obstacles.size();
+        // min # of side jumps at each row and each non-obstacle lane
+        vector<array<int, 3>> dp(N + 1, {INT_MAX, INT_MAX, INT_MAX});
+        // all lanes open in  last row
+        for (int j = 0; j < 3; j++) dp[N - 1][j] = 0;
+
+        // dynamic programming (bottom-up approach)
+        for (int i = N - 2; i >= 0; i--) {
+            // lanes open to a forward jump
+            for (int lane = 0; lane < 3; lane++) {
+                if (lane + 1 == obstacles[i]) continue; // skip if obstacle
+                if (lane + 1 != obstacles[i + 1]) dp[i][lane] = dp[i + 1][lane];
             }
-            // printf("(%i): [%i,%i,%i]\n", i, dp[i][0], dp[i][1], dp[i][2]);
+            // lanes that need side jumps
+            for (int lane = 0; lane < 3; lane++) {
+                if (lane + 1 == obstacles[i]) continue; // skip if obstacle
+                if (dp[i][lane] == INT_MAX) {
+                    dp[i][lane] = 1 + *min_element(dp[i].begin(), dp[i].end());
+                }
+            }
         }
-        return dp.front()[1];
+
+        // entry point is first row second lane
+        return dp[0][1];
     }
 };
