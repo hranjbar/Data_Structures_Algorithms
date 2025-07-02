99eac70ea61528f7080a9833934545fbe01cfe68
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-02 15:26:29 -0500

    Leetcode # 704. Binary Search Template

--- Algorithms/Binary Search/bin_srch_template.cpp
+++ Algorithms/Binary Search/bin_srch_template.cpp
@@ -2,25 +2,19 @@
 Given an array of integers nums which is sorted in ascending order, 
 and an integer target, write a function to search target in nums. 
 If target exists, then return its index. Otherwise, return -1.
-
 You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 Example 1:
-
 Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
 Explanation: 9 exists in nums and its index is 4
-Example 2:
 
+Example 2:
 Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1
- 
 
 Constraints:
-
 1 <= nums.length <= 104
 -104 < nums[i], target < 104
 All the integers in nums are unique.
@@ -31,13 +25,12 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int l = 0, r = nums.size() - 1, mid;
-        while (l < r) {
+        while (l < r){
             mid = l + (r - l) / 2;
-            if (target <= nums[mid]) r = mid;
-            else l = mid + 1;
+            if (target > nums[mid]) l = mid + 1;
+            else r = mid;
         }
-        if (nums[l] == target) return l;
-        else return -1;
+        return target == nums[l] ? l : -1;
     }
 };
 
