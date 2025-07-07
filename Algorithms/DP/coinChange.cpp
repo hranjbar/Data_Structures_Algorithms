facdfa00d69689525adb33f56f0848cf1abcfa64
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-07 15:53:10 -0500

    Leetcode # 322. Coin Change

--- Algorithms/DP/coinChange.cpp
+++ Algorithms/DP/coinChange.cpp
@@ -1,4 +1,4 @@
-'''
+/*
 You are given an integer array coins representing coins of different
 denominations and an integer amount representing a total amount 
 of money.
@@ -16,24 +16,30 @@ Explanation: 11 = 5 + 5 + 1
 Example 2:
 Input: coins = [2], amount = 3
 Output: -1
-Example 3:
 
+Example 3:
 Input: coins = [1], amount = 0
 Output: 0
 
 Constraints:
-
 1 <= coins.length <= 12
 1 <= coins[i] <= 231 - 1
 0 <= amount <= 104
-'''
+*/
 
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount+1 for _ in range(amount+1)]
-        dp[0] = 0
-        for i in range(1, amount+1):
-            for coin in coins:
-                if i >= coin:
-                    dp[i] = min(dp[i], 1 + dp[i - coin])
-        return dp[-1] if dp[-1] <= amount else -1
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        constexpr int LOCAL_INT_MAX{ 10'001 };
+        vector<int> dp(amount + 1, LOCAL_INT_MAX);
+        dp.front() = 0;
+        for (int i = 1; i <= amount; i++){
+            for (int& coin : coins){
+                if (i >= coin){
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return dp.back() < LOCAL_INT_MAX ? dp.back() : -1;
+    }
+};
