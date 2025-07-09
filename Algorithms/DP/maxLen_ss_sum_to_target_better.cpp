65916b0a415bff00b8eb1b4464b91e97147b6710
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-09 16:14:31 -0500

    Leetcode # 2915. Length of the Longest Subsequence that Sums to Target

--- Algorithms/DP/maxLen_ss_sum_to_target_better.cpp
+++ Algorithms/DP/maxLen_ss_sum_to_target_better.cpp
@@ -1,31 +1,30 @@
 /*
 You are given a 0-indexed array of integers nums, and an integer target.
-
-Return the length of the longest subsequence of nums that sums up to target. If no such subsequence exists, return -1.
-
-A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
-
- 
+Return the length of the longest subsequence of nums that sums up to target. 
+If no such subsequence exists, return -1.
+A subsequence is an array that can be derived from another array by deleting 
+some or no elements without changing the order of the remaining elements.
 
 Example 1:
-
 Input: nums = [1,2,3,4,5], target = 9
 Output: 3
-Explanation: There are 3 subsequences with a sum equal to 9: [4,5], [1,3,5], and [2,3,4]. The longest subsequences are [1,3,5], and [2,3,4]. Hence, the answer is 3.
-Example 2:
+Explanation: There are 3 subsequences with a sum equal to 9: [4,5], [1,3,5], 
+and [2,3,4]. The longest subsequences are [1,3,5], and [2,3,4]. Hence, the 
+answer is 3.
 
+Example 2:
 Input: nums = [4,1,3,2,1,5], target = 7
 Output: 4
-Explanation: There are 5 subsequences with a sum equal to 7: [4,3], [4,1,2], [4,2,1], [1,1,5], and [1,3,2,1]. The longest subsequence is [1,3,2,1]. Hence, the answer is 4.
-Example 3:
+Explanation: There are 5 subsequences with a sum equal to 7: [4,3], [4,1,2], 
+[4,2,1], [1,1,5], and [1,3,2,1]. The longest subsequence is [1,3,2,1]. 
+Hence, the answer is 4.
 
+Example 3:
 Input: nums = [1,1,5,4,5], target = 3
 Output: -1
 Explanation: It can be shown that nums has no subsequence that sums up to 3.
- 
 
 Constraints:
-
 1 <= nums.length <= 1000
 1 <= nums[i] <= 1000
 1 <= target <= 1000
@@ -34,21 +33,27 @@ Constraints:
 class Solution {
 public:
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        vector<int> dp(target + 1);
-        for (int & num : nums) {
-            for (int j = target; j >= num; j--) {
-                if (dp[j - num] > 0 || j == num) {
+        vector<int> dp(1 + target, -1);
+        dp.front() = 0; // there exists 0 subsequnce that sums to 0
+        for (int& num : nums){
+            for (int j = target; j >= num; j--){
+                // only if there exists any subsequnce that sums to j-num
+                if (dp[j - num] > -1) { 
                     dp[j] = max(dp[j], 1 + dp[j - num]);
                 }
             }
         }
-        return dp[target] > 0 ? dp[target] : -1;
+        return dp.back();
     }
 };
 
 /*
-The DP array is intialized with default value of zero. 
-For every elemen of nums, i.e. `num`, we update elements of DP:
-Elements of DP are updated only if for each element, `num` times less than that element, DP is not zero, 
-or for index equal to `num`
+The DP array is initialized to -1. 
+For every elemen of nums, i.e. 'num':
+Iterate over values from target down to num (j = {target, target-1, ..., num} 
+and index of DP ix = j - num, so that ix + num = j. Finally we will return DP at 
+ix = target
+Elements of DP are updated only for elements of DP greater than -1, i.e.
+either initially set for index=0, or an index ix where we've already found a subsequence of nums that 
+sums to ix
 */
