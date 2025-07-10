47de450b6052a7c50a04494694bdcfed2e71bd51
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-10 14:57:52 -0500

    Leetcode # 152. Maximum Product Subarray

--- Algorithms/DP/maxProdSubArray.cpp
+++ Algorithms/DP/maxProdSubArray.cpp
@@ -1,25 +1,24 @@
 /*
-Given an integer array nums, find a 
-subarray
- that has the largest product, and return the product.
-
+Given an integer array nums, find a subarray that has the largest product, and return the product.
 The test cases are generated so that the answer will fit in a 32-bit integer.
 
- 
-
 Example 1:
-
 Input: nums = [2,3,-2,4]
 Output: 6
 Explanation: [2,3] has the largest product 6.
-Example 2:
 
+Example 2:
 Input: nums = [-2,0,-1]
 Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+Constraints:
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
 */
 
-lass Solution {
+class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int curmin = 1, curmax = 1, ans = -10, temp;    // ans = -10 because -10 <= nums[i] <= 10
