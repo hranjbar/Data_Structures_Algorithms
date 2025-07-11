6de2beab7ed9fb35b5e6b1a42563725e04a49d1e
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-11 14:41:50 -0500

    Leetcode # 912. Sort an Array

--- Algorithms/Heap Sort/heapSortHeapify.cpp
+++ Algorithms/Heap Sort/heapSortHeapify.cpp
@@ -8,41 +8,31 @@ time complexity and with the smallest space complexity possible.
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        // vector<int> sorted;
-        int len = nums.size();
-        // for (int& n : nums) insert(n, sorted, len);
-        for (int j = len - 1; j >= 0; j--) heapify(nums, j);
-        for (int j = nums.size() - 1; j >= 0; j--) {
-            int num = pop(nums, len);
-            nums[j] = num;
-        }
+        // 1. Heapify O(n log n)
+        for (int i=nums.size()-1; i>=0; i--) sink(nums, i, nums.size()); // note end
+
+        // 2. in-place sorting O(n log n)
+        for (int i=nums.size()-1; i>=0; i--){
+            swap(nums[0], nums[i]); // ascending order
+            sink(nums, 0, i);   // note end
+        }        
         return nums;
     }
-private:
-    // implement max heap
-    void swim(vector<int>& nums, int& len)
+    private:
+
+    // max-heap sink
+    void sink(vector<int>& nums, int st, int end)
     {
-        int i = len - 1;
-        while (i >= 0) {
-            int root = (i - 1) / 2;
-            if (nums[i] > nums[root]) {
-                swap(nums[i], nums[root]);
-                i = root;
-            } else break;
-        }
-    }
-    void sink(vector<int>& nums, int& len)
-    {
-        int i = 0;
-        while (i < len) {
+        int i = st;
+        while (i < end) {
             int l = 2 * i + 1, r = 2 * i + 2;
             int bigger;
-            if (l < len && r < len) {
+            if (l < end && r < end) {
                 if (nums[l] > nums[r]) bigger = l;
                 else bigger = r;
-            } else if (l < len) {
+            } else if (l < end) {
                 bigger = l;
-            } else if (r < len) {
+            } else if (r < end) {
                 bigger = r;
             } else break;
             if (nums[bigger] > nums[i]) {
@@ -50,39 +40,5 @@ private:
                 i = bigger;
             } else break;
         }
-    }
-    void heapify(vector<int>& nums, int i)
-    {
-        int len = nums.size();
-        while (i < len) {
-            int l = 2 * i + 1, r = 2 * i + 2;
-            int bigger;
-            if (l < len && r < len) {
-                if (nums[l] > nums[r]) bigger = l;
-                else bigger = r;
-            } else if (l < len) {
-                bigger = l;
-            } else if (r < len) {
-                bigger = r;
-            } else break;
-            if (nums[bigger] > nums[i]) {
-                swap(nums[bigger], nums[i]);
-                i = bigger;
-            } else break;
-        }
-    }
-    void insert(int num, vector<int>& nums, int& len)
-    {
-        nums.push_back(num);
-        len++;
-        swim(nums, len);
-    }
-    int pop(vector<int>& nums, int& len)
-    {
-        int ret = nums[0];
-        nums[0] = nums[len - 1];
-        len--;
-        sink(nums, len);
-        return ret;
     }
 };
