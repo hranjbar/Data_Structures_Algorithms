c135179afc6c768746dfdd523000ba72ef467a07
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-16 16:06:37 -0500

    Leetcode # 39. Combination Sum

--- Algorithms/Back Tracking/combinationSumDistinctElements.cpp
+++ Algorithms/Back Tracking/combinationSumDistinctElements.cpp
@@ -1,31 +1,62 @@
 /*
-Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+Given an array of distinct integers candidates and a target integer target, 
+return a list of all unique combinations of candidates where the chosen 
+numbers sum to target. You may return the combinations in any order.
+The same number may be chosen from candidates an unlimited number of times. 
+Two combinations are unique if the frequency of at least one of the chosen 
+numbers is different.
+The test cases are generated such that the number of unique combinations 
+that sum up to target is less than 150 combinations for the given input.
 
-The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
-frequency
- of at least one of the chosen numbers is different.
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used 
+multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
 
-The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+
+Constraints:
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40
 */
 
 class Solution {
-    vector<vector<int>> ans;
-    int t;
-    void bt(vector<int> & a, int ix = 0, vector<int> subset = {}, int sum = 0) {
-        if (ix == a.size() || sum >= t) {
-            if (sum == t) ans.push_back(subset);
-        }
-        else {
-            subset.push_back(a[ix]);
-            bt(a, ix, subset, sum + a[ix]);
-            subset.pop_back();
-            bt(a, ix + 1, subset, sum);
-        }
-    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        t = target;
-        bt(candidates);
-        return ans;
+        target_ = target;
+        bt(candidates, 0, 0);
+        return combs_;
+    }
+    private:
+    int target_;
+    vector<vector<int>> combs_;
+    void bt(vector<int>& nums, int idx, int rt, vector<int> ss = {})
+    {
+        if (idx < nums.size() and rt < target_){
+
+            // 1. Skip current candidate
+            bt(nums, idx + 1, rt, ss);
+
+            // 2. Use current candidate and use again
+            ss.push_back(nums[idx]);
+            bt(nums, idx, rt + nums[idx], ss);
+        }
+        else {
+            if (rt == target_){
+                combs_.push_back(ss);
+            }
+        }
     }
 };
