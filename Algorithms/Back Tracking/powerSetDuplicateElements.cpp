d22bd94c3e079c5a17c54408a5b3158255c1fa0d
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-16 16:37:10 -0500

    Leetcode # 90. Subsets II

--- Algorithms/Back Tracking/powerSetDuplicateElements.cpp
+++ Algorithms/Back Tracking/powerSetDuplicateElements.cpp
@@ -1,26 +1,45 @@
 /*
-Given an integer array nums that may contain duplicates, return all possible 
-subsets
- (the power set).
+Given an integer array nums that may contain duplicates, 
+return all possible subsets (the power set).
 
-The solution set must not contain duplicate subsets. Return the solution in any order.
+The solution set must not contain duplicate subsets. 
+Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+ 
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
 */
 
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void bk(vector<int> & nums, int l = 0, vector<int> subset = {}){
-        ans.push_back(subset);
-        for (int i = l ; i < nums.size() ; i++){
-            if (i > l && nums[i] == nums[i-1]) continue;
-            subset.push_back(nums[i]);
-            bk(nums, i + 1, subset);
-            subset.pop_back();
-        }
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        bk(nums);
-        return ans;
+        bt(nums, 0, {});
+        return pset_;
+    }
+    vector<vector<int>> pset_;
+    void bt(vector<int>& nums, int idx, vector<int> ss)
+    {
+        if (idx < nums.size()){
+
+            // 1. Skip to next distinct num
+            int cur = nums[idx], j = idx;
+            while (j < nums.size() and cur == nums[j]) j++;
+            bt(nums, j, ss);
+
+            // 2. Use and got to next
+            ss.push_back(nums[idx]);
+            bt(nums, idx + 1, ss);
+         
+        }
+        else pset_.push_back(ss);
     }
 };
