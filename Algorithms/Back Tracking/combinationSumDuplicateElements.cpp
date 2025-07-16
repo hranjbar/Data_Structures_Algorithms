268beb3248f625ec43477ec07b961ba1c4c78eeb
Homayoon Ranjbar <horanj.1022@gmail.com>
2025-07-16 16:00:23 -0500

    Leetcode # 40. Combination Sum II

--- Algorithms/Back Tracking/combinationSumDuplicateElements.cpp
+++ Algorithms/Back Tracking/combinationSumDuplicateElements.cpp
@@ -1,30 +1,60 @@
 /*
-Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
-
+Given a collection of candidate numbers (candidates) and a target number (target), 
+find all unique combinations in candidates where the candidate numbers sum to target.
 Each number in candidates may only be used once in the combination.
-
 Note: The solution set must not contain duplicate combinations.
+
+Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+
+Example 2:
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+
+Constraints:
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
 */
 
 class Solution {
-    vector<vector<int>> ans;
-    void bt(vector<int> & a, int rem, int ix = 0, vector<int> subset = {}) {
-        if (ix == a.size() || rem <= 0) {
-            if (rem == 0) ans.push_back(subset);
-        }
-        else {
-            subset.push_back(a[ix]);
-            bt(a, rem - a[ix], ix + 1, subset);
-            subset.pop_back();
-            int cur = a[ix];
-            do {ix++;} while (ix < a.size() && a[ix] == cur);
-            bt(a, rem, ix, subset);
-        }
-    }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        target_ = target;
         sort(candidates.begin(), candidates.end());
-        bt(candidates, target);
-        return ans;
+        bt(candidates, 0, 0, {});
+        return combs_;
+    }
+    private:
+    vector<vector<int>> combs_;
+    int target_;
+    void bt(vector<int>& nums, int idx, int sum, vector<int> ss)
+    {
+        if (idx < nums.size() and sum < target_){
+
+            // 1. skip current
+            int cur = nums[idx], j = idx;
+            while (j < nums.size() and nums[j] == cur) j++;
+            bt(nums, j, sum, ss);   // go to a new distinct one
+            
+            // 2. use then go to next
+            ss.push_back(nums[idx]);
+            bt(nums, idx + 1, sum + nums[idx], ss);
+            
+        }
+        else {
+            if (sum == target_) combs_.push_back(ss);
+        }
     }
 };
